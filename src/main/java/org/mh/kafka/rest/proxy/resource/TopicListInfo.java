b6571c2ff47d60f421eb2990a1ffae37e9b7fa64
Markus Helbig <mhelbig81@googlemail.com>
2016-09-30 19:22:45 +0200

    since consumer - the only one giving available topics - is not thread-safe, update topic-list every 2 sec and just return current available result

---
+++ src/main/java/org/mh/kafka/rest/proxy/resource/TopicListInfo.java
@@ -0,0 +1,57 @@
+/*
+ *  Copyright 2016 Markus Helbig
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.mh.kafka.rest.proxy.resource;
+
+import com.google.common.collect.Lists;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_SINGLETON;
+
+/**
+ * Created by markus on 30/09/2016.
+ */
+@Component
+@EnableScheduling
+@Scope(value = SCOPE_SINGLETON)
+class TopicListInfo {
+
+    @Autowired
+    private Consumer<String, String> consumer;
+
+    private List<String> topcis;
+
+    public TopicListInfo() {
+        topcis = Lists.newArrayList();
+    }
+
+    @Scheduled(fixedDelay = 2000)
+    private void updateTopicList() {
+        topcis.clear();
+        topcis.addAll(consumer.listTopics().keySet());
+    }
+
+    List<String> getTopcis() {
+        return topcis;
+    }
+}
